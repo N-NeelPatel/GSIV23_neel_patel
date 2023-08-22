3a560b58b4333f0f27062bee3a65384fde6f719e
Neel Patel <neel199907@gmail.com>
2023-08-22 19:41:48 +0530

    changes for movie detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./components/MovieCard";
 import Search from "./components/Search";
 import EmptyState from "./components/EmptyState";
 import Detail from "./components/Detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
-import { getUpcomingMovies, getUpcomingMoviesPage, getSearchResult } from "./utils/api";
+import {
+  getUpcomingMovies,
+  getUpcomingMoviesPage,
+  getSearchResult,
+} from "./utils/api";
 
 function App() {
   const [movieData, setMoviedata] = useState(null);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const [isSearching, setIsSearching] = useState(false); 
-  const [searchResults, setSearchResults] = useState([]); 
-
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     getUpcomingMovies()
@@ -68,34 +72,49 @@ function App() {
 
   const moviesToDisplay = isSearching ? searchResults : movieData;
 
+  console.log('movieData', movieData);
+
   return (
-    <>
-      <div className="top-navigation flex justify-between items-center p-3 shadow-lg min-w-[512px]">
-        <Search onSearch={handleSearch} />
-        <AiFillHome className="text-[#4A4A4A] mr-3" />
-      </div>
-      <div className="flex flex-wrap p-3">
-        {!moviesToDisplay || moviesToDisplay.length === 0 ? (
-          <EmptyState />
-        ) : (
-          moviesToDisplay.map((movie) => (
-            <div className="pb-3 pr-3" key={movie.id}>
-              <MovieCard movieData={movie} />
-            </div>
-          ))
-        )}
-      </div>
-      {!isSearching && moviesToDisplay && moviesToDisplay.length > 0 && (
-        <div className="flex justify-center p-3">
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-            onClick={handleLoadMoreClick}
-          >
-            Load More
-          </button>
+    <Router>
+      <>
+        <div className="top-navigation flex justify-between items-center p-3 shadow-lg min-w-[512px]">
+          <Search onSearch={handleSearch} />
+          <Link to="/">
+            <AiFillHome className="text-[#4A4A4A] mr-3" />
+          </Link>
         </div>
-      )}
-    </>
+        <div className="flex flex-wrap p-3">
+        <Switch>
+          <Route exact path={"/"}>
+          {!moviesToDisplay || moviesToDisplay.length === 0 ? (
+            <EmptyState />
+          ) : (
+            moviesToDisplay.map((movie) => (
+              <div className="pb-3 pr-3" key={movie.id}>
+                <Link to={`/movie/${movie.id}`}>
+                  <MovieCard movieData={movie} />
+                </Link>
+              </div>
+            ))
+          )}
+          </Route>
+          <Route path="/movie/:id">
+            <Detail />
+          </Route>
+        </Switch>
+        </div>
+        {!isSearching && moviesToDisplay && moviesToDisplay.length > 0 && (
+          <div className="flex justify-center p-3">
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+              onClick={handleLoadMoreClick}
+            >
+              Load More
+            </button>
+          </div>
+        )}
+      </>
+    </Router>
   );
 }
 
--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,23 +1,20 @@
 import { useState, useEffect } from "react";
 import { getSingleMovieInfo, getMovieCredits } from "../utils/api";
+import { useParams } from "react-router-dom";
 
-export default function Detail({ movieData }) {
+export default function Detail() {
+  const { id } = useParams();
   const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
-  const overview = movieData.overview;
-  const movieRating = movieData.vote_average * 10;
-  const movieTitle = movieData.title;
 
   const [movieDetail, setMoviedetail] = useState(null);
   const [error, setError] = useState(null);
   const [castDetail, setCastdetail] = useState(null);
   const [directorName, setDirectorname] = useState(null);
 
-  const movieId = "976573"; // test id, need to change this
-
   useEffect(() => {
     const fetchMovieCredits = async () => {
       try {
-        const response = await getMovieCredits(movieId);
+        const response = await getMovieCredits(id);
         setCastdetail(response.data);
       } catch (error) {
         setError(error);
@@ -26,7 +23,7 @@ export default function Detail({ movieData }) {
 
     const fetchMovieInfo = async () => {
       try {
-        const response = await getSingleMovieInfo(movieId);
+        const response = await getSingleMovieInfo(id);
         setMoviedetail(response.data);
       } catch (error) {
         setError(error);
@@ -35,7 +32,7 @@ export default function Detail({ movieData }) {
 
     fetchMovieCredits();
     fetchMovieInfo();
-  }, [movieId]);
+  }, [id]);
 
   useEffect(() => {
     const findDirector = () => {
@@ -54,42 +51,46 @@ export default function Detail({ movieData }) {
 
   return (
     <>
-      <div className="flex">
-        <img
-          src={`${imageBaseUrl}${movieData.poster_path}`}
-          className="h-[250px] w-[200px]"
-        />
-        <div className="ml-4">
-          <div className="flex items-center">
-            <div className="text-xl font-semibold">{movieTitle}</div>
-            <div
-              className={`${
-                movieRating >= 70
-                  ? "text-green-500"
-                  : movieRating >= 50
-                  ? "text-yellow-500"
-                  : "text-red-500"
-              } ml-4 mt-[1px]`}
-            >
-              ({movieRating}%)
+      {movieDetail && (
+        <div className="flex">
+          <img
+            src={`${imageBaseUrl}${movieDetail.poster_path}`}
+            className="h-[250px] w-[200px]"
+          />
+          <div className="ml-4">
+            <div className="flex items-center">
+              <div className="text-xl font-semibold">{movieDetail.title}</div>
+              <div
+                className={`${
+                  Math.round(movieDetail.vote_average * 10) >= 70
+                    ? "text-green-500"
+                    : Math.round(movieDetail.vote_average * 10) >= 50
+                    ? "text-yellow-500"
+                    : "text-red-500"
+                } ml-4 mt-[1px]`}
+              >
+                ({Math.round(movieDetail.vote_average * 10)}%)
+              </div>
+            </div>
+            <div className="mt-2">
+              {movieDetail.release_date.substring(0, 4)} |{" "}
+              {movieDetail ? movieDetail.runtime : ""} min | {directorName}
+            </div>
+            <div className="leading-5 mt-2">
+              Cast:{" "}
+              {castDetail &&
+                castDetail.cast
+                  .slice(0, 20)
+                  .map((cast) => cast.name)
+                  .join(", ")}
+              {castDetail && castDetail.cast.length > 20 ? " ..." : ""}
+            </div>
+            <div className="leading-5 mt-2">
+              Description: {movieDetail.overview}
             </div>
           </div>
-          <div className="mt-2">
-            {movieData.release_date.substring(0, 4)} |{" "}
-            {movieDetail ? movieDetail.runtime : ""} min | {directorName}
-          </div>
-          <div className="leading-5 mt-2">
-            Cast:{" "}
-            {castDetail &&
-              castDetail.cast
-                .slice(0, 20)
-                .map((cast) => cast.name)
-                .join(", ")}
-            {castDetail && castDetail.cast.length > 20 ? " ..." : ""}
-          </div>
-          <div className="leading-5 mt-2">Description: {overview}</div>
         </div>
-      </div>
+      )}
     </>
   );
 }
